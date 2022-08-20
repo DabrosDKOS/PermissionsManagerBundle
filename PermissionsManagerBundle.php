c372736841786101e62f870648ec595ef525fa2d
mdabrowski <mdabrowski@inelo.pl>
2022-08-20 22:15:24 +0200

    start

--- PermissionsManagerBundle.php
+++ PermissionsManagerBundle.php
@@ -1,12 +1,17 @@
 <?php
+
 namespace DabrosDkos\PermissionsManager;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * PermissionsManagerBundle
  */
 class PermissionsManagerBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
 }
