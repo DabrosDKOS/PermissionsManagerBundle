2a862d3608ffb2440684322bedd1893033958db1
mdabrowski <mdabrowski@inelo.pl>
2022-08-21 18:42:34 +0200

    start

--- PermissionsManagerBundle.php
+++ PermissionsManagerBundle.php
@@ -2,6 +2,8 @@
 
 namespace DabrosDkos\PermissionsManagerBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,4 +11,25 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PermissionsManagerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+        if (class_exists($ormCompilerClass)) {
+            $namespaces = array('DabrosDkos\PermissionsManagerBundle\Entity');
+            $directories = array(realpath(__DIR__ . '/Entity'));
+            $managerParameters = array();
+            $enabledParameter = false;
+            $aliasMap = array('PermissionsManagerBundle' => 'DabrosDkos\PermissionsManagerBundle\Entity');
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createAnnotationMappingDriver(
+                    $namespaces,
+                    $directories,
+                    $managerParameters,
+                    $enabledParameter,
+                    $aliasMap
+                )
+            );
+        }
+    }
 }
