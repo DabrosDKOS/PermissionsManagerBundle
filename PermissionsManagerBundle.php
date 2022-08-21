60bb40c4b60361b4095f3b9d448e54d367f4a1ad
mdabrowski <mdabrowski@inelo.pl>
2022-08-21 20:36:08 +0200

    start

--- PermissionsManagerBundle.php
+++ PermissionsManagerBundle.php
@@ -14,22 +14,22 @@ class PermissionsManagerBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
-        if (class_exists($ormCompilerClass)) {
-            $namespaces = array('DabrosDkos\PermissionsManagerBundle\Model');
-            $directories = array(realpath(__DIR__ . '/Entity'));
-            $managerParameters = array();
-            $enabledParameter = false;
-            $aliasMap = array('PermissionsManagerBundle' => 'DabrosDkos\PermissionsManagerBundle\Model');
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createAnnotationMappingDriver(
-                    $namespaces,
-                    $directories,
-                    $managerParameters,
-                    $enabledParameter,
-                    $aliasMap
-                )
-            );
-        }
+//        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+//        if (class_exists($ormCompilerClass)) {
+//            $namespaces = array('DabrosDkos\PermissionsManagerBundle\Model');
+//            $directories = array(realpath(__DIR__ . '/Entity'));
+//            $managerParameters = array();
+//            $enabledParameter = false;
+//            $aliasMap = array('PermissionsManagerBundle' => 'DabrosDkos\PermissionsManagerBundle\Model');
+//            $container->addCompilerPass(
+//                DoctrineOrmMappingsPass::createAnnotationMappingDriver(
+//                    $namespaces,
+//                    $directories,
+//                    $managerParameters,
+//                    $enabledParameter,
+//                    $aliasMap
+//                )
+//            );
+//        }
     }
 }
