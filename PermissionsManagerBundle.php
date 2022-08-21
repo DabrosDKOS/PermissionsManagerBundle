7ed3a547e42c7e20025f3bd8729b1bbe3a8774aa
mdabrowski <mdabrowski@inelo.pl>
2022-08-21 20:30:34 +0200

    start

--- PermissionsManagerBundle.php
+++ PermissionsManagerBundle.php
@@ -16,11 +16,11 @@ class PermissionsManagerBundle extends Bundle
         parent::build($container);
         $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
         if (class_exists($ormCompilerClass)) {
-            $namespaces = array('DabrosDkos\PermissionsManagerBundle\Entity');
+            $namespaces = array('DabrosDkos\PermissionsManagerBundle\Model');
             $directories = array(realpath(__DIR__ . '/Entity'));
             $managerParameters = array();
             $enabledParameter = false;
-            $aliasMap = array('PermissionsManagerBundle' => 'DabrosDkos\PermissionsManagerBundle\Entity');
+            $aliasMap = array('PermissionsManagerBundle' => 'DabrosDkos\PermissionsManagerBundle\Model');
             $container->addCompilerPass(
                 DoctrineOrmMappingsPass::createAnnotationMappingDriver(
                     $namespaces,
